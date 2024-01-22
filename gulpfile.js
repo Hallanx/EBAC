b2e162fffae477024b0bb217a5b16169e85b9a78
ALAN VAZ CARDOSO <145813724+Hallanx@users.noreply.github.com>
2024-01-22 13:56:12 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src('./souce/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('.buld/images'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gup.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('./souce/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+exports.default = function () {
+    exports.watch('./souce/styles/*.scss', {
+        ignoreInitial: false
+    }, gulp.series(compilaSass));
+    exports.watch('./source/scripts/*.js', {
+        ignoreInitial: false
+    }, gulp.series(comprimeJavaScript));
+    exports.watch('./souce/images/*', {
+        ignoreInitial: false
+    }, gulp.series(comprimeImagens));
+}
